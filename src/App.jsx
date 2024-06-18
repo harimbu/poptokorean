afbecc5a728050d46c7812f9befbd30a98b2943d
harimbu <harimbu@gmail.com>
2024-06-18 17:23:47 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,152 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>pop</div>
-    </>
+    <div className='container'>
+      <header>
+        <div className='inner'>
+          <h1>🌳 팝송가사 </h1>
+          <input type='text' placeholder='Search song...' />
+        </div>
+      </header>
+
+      <main>
+        <ul>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>
+              {' '}
+              And I wish time would slow down. So I could keep your And I wish time would slow down. So I could keep
+              your...
+            </div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+          <li>
+            <div className='thumb'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiYYzsaeLVnwKEXkyfZp-Jbgr5oouyK3Eu3g&s'
+                alt=''
+              />
+            </div>
+            <div className='title'>My Funny Valentine</div>
+            <div className='singer'>Chet Baker</div>
+            <div className='text'>And I wish time would slow down. So I could keep your...</div>
+            <button>보기</button>
+          </li>
+        </ul>
+      </main>
+
+      <footer>
+        <div className='inner'>
+          <span>(c) 2024. harimbu@gmail.com</span>
+          <div className='footer_menu'>
+            <span>about</span>
+            <span>login</span>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
